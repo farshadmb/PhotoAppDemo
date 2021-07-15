9ad95acef639a6d022ad3e3c7b2859a4623b852b
Farshad Mousalou <farshadm90@gmail.com>
2021-07-15 18:32:41 +0430

    Initial Commit
    (cherry picked from commit b30a5376e9a7db587b6ad712b2f25e91b776d842)

---
+++ PhotoAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoAppDemo
+//
+//  Created by Farshad Mousalou on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
